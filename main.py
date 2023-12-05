cb424179ee7e1684fd3ccb6077edd25000f348f5
nyxical420 <xaveegacha7@gmail.com>
2023-12-05 20:24:09 +0900

    make server now support yaml

--- main.py
+++ main.py
@@ -2,16 +2,15 @@
 # If you have python installed, you can also use this to make edits and see changes easily without waiting for github's cache.
 
 # Copy this to install required libraries:
-# pip install fastapi uvicorn
+# pip install fastapi uvicorn PyYAML
 
-import yaml
 from jinja2 import Template
+from yaml import load, FullLoader
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
-from json import load
 from uvicorn import run
 
 app = FastAPI(
@@ -29,7 +28,7 @@ async def wiki(request: Request, wikipath: str = ""):
 
     try:
         with open(f"./pagedata/{wikipath}.yaml", "r") as file:
-            data = yaml.load(file, Loader=yaml.FullLoader)
+            data = load(file, Loader=FullLoader)
     except FileNotFoundError: data = ""
 
     html_template = Template(html.read())
