f613f6dda72f8c0958853297ff24bb1f2b33efbe
starboard <c0la@inorbit.com>
2023-12-24 18:16:51 +0000

    ho my god

--- assets/scripts/wave.js
+++ assets/scripts/wave.js
@@ -1,5 +1,5 @@
-window.addEventListener("load", () => {
-    document.querySelectorAll(".float").forEach(el => {
+setInterval(() => {
+    document.querySelectorAll("float").forEach(el => {
         const string = el.innerHTML
         const nodeName = el.nodeName
         const matchedTags = string.match(/<[^>]*>/g)
@@ -26,7 +26,7 @@ window.addEventListener("load", () => {
             }
     
             if(!ignore.includes(parseInt(i))) {
-                processedString += `<${nodeName} style='display: inline-block; animation: wave 1s ease-in-out infinite; animation-delay: ${offset}ms; ${extraStyles}'>${textArray[i]}</${nodeName}>`
+                processedString += `<${nodeName}-child style='display: inline-block; font-size: inherit; animation: wave 1s ease-in-out infinite; animation-delay: ${offset}ms; ${extraStyles}'>${textArray[i]}</${nodeName}-child>`
                 offset += 75
             }
         }
@@ -36,4 +36,4 @@ window.addEventListener("load", () => {
         console.log(string)
         el.innerHTML = processedString
     })
-})
+}, 1333)
